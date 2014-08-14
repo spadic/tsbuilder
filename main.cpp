a3dd38490c6c2ec04d502c257ce9c29917080f16
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-08-14 16:46:20 +0200

    continued tscreator, now it at least produces a seemingly valid .tsa file, but is not at all user-controllable

--- main.cpp
+++ main.cpp
@@ -4,21 +4,41 @@
 int main()
 {
 
-const int NUM_TIMESLICES {10};
-const int NUM_COMPONENTS {1};
-const int NUM_MICROSLICES {100};
+const size_t NUM_TIMESLICES {10};
+const size_t NUM_COMPONENTS {1};
+const size_t NUM_MICROSLICES {100};
+
+const uint8_t SYS_ID {0x40}; // TRD
+const uint8_t SYS_VER {0x01};
+
+const size_t SIZE {20}; // TODO
 
 fles::TimesliceOutputArchive ar {"output.tsa"};
 
-for (int ts_index {0}; ts_index < NUM_TIMESLICES; ts_index++) {
+for (uint64_t ts_index {0}; ts_index < NUM_TIMESLICES; ts_index++) {
     fles::StorableTimeslice ts {NUM_MICROSLICES};
 
-    for (int comp_index {0}; comp_index < NUM_COMPONENTS; comp_index++) {
+    for (uint16_t comp_index {0}; comp_index < NUM_COMPONENTS; comp_index++) {
         ts.append_component(NUM_MICROSLICES, ts_index);
 
-        for (int mc_index {0}; mc_index < NUM_MICROSLICES; mc_index++) {
-            fles::MicrosliceDescriptor mc_desc; // TODO
-            uint8_t *mc_content; // TODO
+        for (uint64_t mc_index {0}; mc_index < NUM_MICROSLICES; mc_index++) {
+            auto mc_size = SIZE + ((~SIZE + 1) % 8); // round up to multiple of 8
+            fles::MicrosliceDescriptor mc_desc {
+                0xDD, // hdr_id
+                0x01, // hdr_ver
+                comp_index, // eq_id
+                0, // flags
+                SYS_ID, // sys_id TODO
+                SYS_VER, // sys_ver TODO
+                mc_index, // idx TODO
+                0, // crc
+                mc_size, // size TODO
+                0 // offset -> overwritten by StorableTimeslice::append_microslice
+            };
+            uint8_t mc_content[mc_size]; // TODO
+            for (uint8_t i {0}; i < mc_size; i++) {
+                mc_content[i] = i;
+            }
             ts.append_microslice(comp_index, mc_index, mc_desc, mc_content);
         }
     }
