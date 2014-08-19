c541f0e906495ca2f26e9c07b207e5978292a77f
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-08-19 16:21:17 +0200

    save work in progress

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include "StorableTimeslice.hpp"
 #include "TimesliceOutputArchive.hpp"
 #include <unordered_map>
+#include <vector>
+#include <cstdio>
 
-int main()
-{
 
-const size_t NUM_TIMESLICES {10};
 const size_t NUM_COMPONENTS {1};
-const size_t NUM_MICROSLICES {100};
+const size_t NUM_MICROSLICES {100}; // total, over all timeslices
+const size_t TIMESLICE_LENGTH {10}; // microslices per timeslice
 
 const uint16_t EQ_ID[NUM_COMPONENTS] {1};
 const uint8_t SYS_ID[NUM_COMPONENTS] {0x40}; // TRD
@@ -17,36 +17,112 @@ const size_t SIZE {20}; // TODO
 
 fles::TimesliceOutputArchive ar {"output.tsa"};
 
-for (uint64_t ts_index {0}; ts_index < NUM_TIMESLICES; ts_index++) {
-    fles::StorableTimeslice ts {NUM_MICROSLICES};
+//-------------------
 
-// one timeslice per ts_index
+// one timeslice per timeslice index
 std::unordered_map<uint64_t, fles::StorableTimeslice> timeslices;
+
+fles::MicrosliceDescriptor default_mc_descriptor()
+{
+    fles::MicrosliceDescriptor desc;
+    desc.hdr_id = 0xDD;
+    desc.hdr_ver = 0x01;
+    desc.flags = 0;
+    desc.crc = 0;
+    return desc;
+};
+
+struct MicrosliceContainer {
+    fles::MicrosliceDescriptor desc;
+    uint8_t *content;
+};
+
+struct MicrosliceSource {
+    MicrosliceSource(uint16_t eq_id, uint8_t sys_id, uint8_t sys_ver)
+        : _eq_id {eq_id}, _sys_id {sys_id}, _sys_ver {sys_ver} {};
+
+    void add(uint64_t mc_index, std::vector<uint8_t> content)
+    {
+        // what
+        auto desc = _make_desc();
+        desc.size = content.size();
+        MicrosliceContainer mc {desc, content.data()};
+
+        // where
+        auto it = _microslices.find(mc_index);
+        if (it != end(_microslices)) {
+            it->second = mc;
+        } else {
+            _microslices.insert(it, {mc_index, mc});
+        }
+    };
+
+    MicrosliceContainer get(uint64_t mc_index)
+    {
+        auto it = _microslices.find(mc_index);
+        if (it != end(_microslices)) {
+            return it->second;
+        } else {
+            auto desc = _make_desc();
+            desc.size = 0;
+            return {desc, nullptr};
+        }
+    };
+
+private:
+    uint16_t _eq_id;
+    uint8_t _sys_id, _sys_ver;
+    std::unordered_map<uint64_t, MicrosliceContainer> _microslices;
+    fles::MicrosliceDescriptor _make_desc()
+    {
+        auto desc = default_mc_descriptor();
+        desc.eq_id = _eq_id;
+        desc.sys_id = _sys_id;
+        desc.sys_ver = _sys_ver;
+        return desc;
+    };
+};
 
 std::unordered_map<
     uint16_t, // eq_id
-    std::unordered_map<
-        uint64_t, // mc_index
-        std::pair<fles::MicrosliceDescriptor, uint8_t *> // desc, content
-    >
-> microslices;
+    MicrosliceSource
+> mc_sources;
 
-void add_component_from_eq_id(uint16_t eq_id, uint64_t mc_index_start)
+void add_component_from_eq_id(uint16_t eq_id, uint64_t ts_index)
 {
-    uint64_t ts_index = mc_index_start/TIMESLICE_LENGTH;
-    // get timeslice corresponding to ts_index, create if necessary
-    auto& ts = (*timeslices.emplace(ts_index, TIMESLICE_LENGTH).first).second;
-    uint32_t comp_index = ts.append_component(TIMESLICE_LENGTH, ts_index);
-    uint64_t mc_index {mc_index_start};
-    for (uint64_t mc_index {mc_index_start};
-            mc_index < mc_index_start + TIMESLICE_LENGTH; mc_index++) {
-        auto& mc_item = microslices[eq_id][comp_index];
-        ts.append_microslice(comp_index, mc_index, mc_item.first, mc_item.second);
+    // get timeslice corresponding to timeslice index, create if necessary
+    // roughly equivalent to the following Python code:
+    //   if ts_index not in timeslices:
+    //       timeslices[ts_index] = Timeslice(TIMESLICE_LENGTH)
+    //   ts = timeslices[ts_index]
+    // or simply:
+    //   ts = timeslices.setdefault(ts_index, Timeslice(TIMESLICE_LENGTH))
+    auto& ts = timeslices.emplace(ts_index, TIMESLICE_LENGTH).first->second;
+
+    // get component index by creating a new component
+    auto c = ts.append_component(TIMESLICE_LENGTH, ts_index);
+
+    // get {mc_index: (desc, content), ...} object for this eq_id
+    auto& mc_source = mc_sources.at(eq_id);
+
+    // iterate over microslice interval corresponding to timeslice index
+    auto m = ts_index * TIMESLICE_LENGTH;
+    for (auto i = 0; i < TIMESLICE_LENGTH; i++) {
+        auto mc = mc_source.get(m+i);
+        ts.append_microslice(c, m+i, mc.desc, mc.content);
     }
 }
 
-    uint16_t comp_index {0};
-    while (comp_index < NUM_COMPONENTS) {
+//-------------------
+
+int main()
+{
+
+for (uint64_t ts_index {0}; ts_index < NUM_MICROSLICES/TIMESLICE_LENGTH; ts_index++) {
+    fles::StorableTimeslice ts {TIMESLICE_LENGTH};
+
+    uint32_t comp_index;
+    do {
         comp_index = ts.append_component(NUM_MICROSLICES, ts_index);
 
         for (uint64_t mc_index {0}; mc_index < NUM_MICROSLICES; mc_index++) {
@@ -69,10 +145,8 @@ void add_component_from_eq_id(uint16_t eq_id, uint64_t mc_index_start)
             }
             ts.append_microslice(comp_index, mc_index, mc_desc, mc_content);
         }
-    }
+    } while (comp_index+1 < NUM_COMPONENTS);
     ar.write(ts);
 }
-
-return 0;
 
 } // main
