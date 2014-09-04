770b5a2871d9e68b4988e83d8cbd60c8ee5b0319
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-09-04 13:30:24 +0200

    reordered functions to match header

--- TimesliceBuilder.cpp
+++ TimesliceBuilder.cpp
@@ -10,6 +10,26 @@ TimesliceBuilder::TimesliceBuilder(size_t ts_len, uint64_t start_index)
     _last = end(_timeslices); // invalidate
 }
 
+void TimesliceBuilder::add_microslices(MicrosliceSource& mc_source)
+{
+    // calculate the range of timeslices we need to access
+    auto ts_first = mc_source.start_index / _ts_len;
+    auto ts_last = (mc_source.start_index + mc_source.size()) / _ts_len;
+
+    for (auto ts_idx = ts_first; ts_idx < ts_last; ts_idx++) {
+        // get the current timeslice or create it if it doesn't exist
+        auto& ts = _timeslices.emplace(ts_idx, _ts_len).first->second;
+
+        // add all microslices belonging to this timeslice to a new component
+        auto comp_idx = ts.append_component(_ts_len, ts_idx);
+        auto mc_base_idx = ts_idx * _ts_len;
+        for (size_t i = 0; i < _ts_len; i++) {
+            auto mc = mc_source.get(mc_base_idx + i);
+            ts.append_microslice(comp_idx, i, mc);
+        }
+    }
+}
+
 std::unique_ptr<StorableTimeslice> TimesliceBuilder::get()
 {
     auto ts = static_cast<StorableTimeslice *>(nullptr);
@@ -30,26 +50,6 @@ std::unique_ptr<StorableTimeslice> TimesliceBuilder::get()
     }
 
     return std::unique_ptr<StorableTimeslice> {ts};
-}
-
-void TimesliceBuilder::add_microslices(MicrosliceSource& mc_source)
-{
-    // calculate the range of timeslices we need to access
-    auto ts_first = mc_source.start_index / _ts_len;
-    auto ts_last = (mc_source.start_index + mc_source.size()) / _ts_len;
-
-    for (auto ts_idx = ts_first; ts_idx < ts_last; ts_idx++) {
-        // get the current timeslice or create it if it doesn't exist
-        auto& ts = _timeslices.emplace(ts_idx, _ts_len).first->second;
-
-        // add all microslices belonging to this timeslice to a new component
-        auto comp_idx = ts.append_component(_ts_len, ts_idx);
-        auto mc_base_idx = ts_idx * _ts_len;
-        for (size_t i = 0; i < _ts_len; i++) {
-            auto mc = mc_source.get(mc_base_idx + i);
-            ts.append_microslice(comp_idx, i, mc);
-        }
-    }
 }
 
 } // namespace
