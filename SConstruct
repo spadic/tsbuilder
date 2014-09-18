11af40f7eee128bf322781b599e7de2eb4d8e222
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-09-18 14:31:07 +0200

    use a zmq socket to add MicrosliceSources and add DTMs, instead of trying to write a user interface in c++...

--- SConstruct
+++ SConstruct
@@ -13,11 +13,19 @@ except KeyError:
 env.Append(CPPPATH=['$FLES_IPC_DIR'])
 fles_ipc = env.Library(env.Glob('$FLES_IPC_DIR/*.cpp'))
 
+# flib_dpb
+try:
+    env.Replace(FLIB_DPB_DIR=os.environ['FLIB_DPB'])
+except KeyError:
+    raise SystemExit("Please set the FLIB_DPB environment variable "
+                     "to the root of the flib_dpb source tree.")
+env.Append(CPPPATH=['$FLIB_DPB_DIR'])
+flib_dpb = env.Library(env.Glob('$FLIB_DPB_DIR/*.cpp'))
 
 # tsbuilder
 env.Program('tsbuilder',
             ['main.cpp', 'MicrosliceSource.cpp', 'TimesliceBuilder.cpp'],
-            LIBS=[fles_ipc, 'boost_serialization'])
+            LIBS=[fles_ipc, flib_dpb, 'boost_serialization', 'zmq'])
 
 # tstester
 env.Program('tstester', ['tester.cpp'],
--- main.cpp
+++ main.cpp
@@ -1,21 +1,97 @@
-// local
 #include "MicrosliceSource.hpp"
 #include "TimesliceBuilder.hpp"
-// fles_ipc
+
 #include "TimesliceOutputArchive.hpp"
+#include "MicrosliceContents.hpp"
+
+#include <zmq.hpp>
+
+//==== constants =====================================================
+
+const uint16_t QUIT {0xFFFF};
+const uint16_t NEXT_MC {0xFFFE};
+const uint16_t ADD_SRC {0xFFFD};
+const char *IPC_ADDRESS = "ipc:///tmp/tsbuilder.ipc";
+
+//==== helper function ===============================================
+
+void add_microslice_contents(fles::MicrosliceSource& src,
+                             const flib_dpb::MicrosliceContents& mc)
+{
+    auto contents = mc.raw();
+    auto start_p = reinterpret_cast<uint8_t *>(contents.data());
+    auto end_p = start_p + contents.size() * sizeof(*contents.data());
+    src.add({start_p, end_p});
+}
+
+//==== main ==========================================================
 
 int main()
 {
-    auto mc_src = fles::MicrosliceSource {1, 0x40, 0x01, 8};
-    for (size_t i = 0; i < 5000; i++) {
-        auto v = std::vector<uint8_t> (7, i);
-        mc_src.add(v);
+    auto sources = std::vector<fles::MicrosliceSource> {};
+    auto contents = std::vector<flib::MicrosliceContents> {};
+
+    auto context = zmq::context_t {1};
+    auto socket = zmq::socket_t {context, ZMQ_PULL};
+    socket.bind(IPC_ADDRESS);
+
+    //---- main loop: read data from ZMQ socket ----------------------
+
+    auto m = zmq::message_t {};
+    while (true) {
+        // wait for message and read as list of 16 bit words
+        socket.recv(&m);
+        auto data = static_cast<uint16_t *>(m.data());
+        auto n = m.size() / sizeof(*data);
+
+        // first word determines the action
+        if (n < 1) { continue; }
+        auto action = *data++;
+
+        if (action == ADD_SRC) {
+            // [ADD_SRC, eq_id, sys_id, sys_ver, mc_start_idx]
+            if (n < 5) { continue; }
+            auto eq_id = *data++;
+            auto sys_id = uint8_t (*data++);
+            auto sys_ver = uint8_t (*data++);
+            auto mc_start_idx = *data++;
+            sources.emplace_back(eq_id, sys_id, sys_ver, mc_start_idx);
+            contents.emplace_back();
+            continue;
+        }
+        if (action == NEXT_MC) {
+            // [NEXT_MC, src_idx]
+            if (n < 2) { continue; }
+            auto src_idx = *data++;
+            if (!(src_idx < sources.size())) { continue; }
+            add_microslice_contents(sources[src_idx], contents[src_idx]);
+            contents[src_idx] = flib::MicrosliceContents {};
+        }
+        if (action == QUIT) {
+            // [QUIT]
+            for (size_t i = 0; i < sources.size(); i++) {
+                add_microslice_contents(sources[i], contents[i]);
+            }
+            break;
+        }
+
+        // default action: add new DTM
+        // [---, src_idx, cbm_addr, d0, d1, ...]
+        if (n < 3) { continue; }
+        auto src_idx = *data++;
+        if (!(src_idx < sources.size())) { continue; }
+        auto cbm_addr = *data++;
+        sources[src_idx].add_dtm({cbm_addr, data, n-3});
     }
 
-    auto bld = fles::TimesliceBuilder {10, 15};
-    bld.add_microslices(mc_src);
+    //---- after the data is accumulated: write timeslices to file ---
 
-    fles::TimesliceOutputArchive ar {"output.tsa"};
+    auto bld = fles::TimesliceBuilder {10, 0}; // TODO from argv
+    for (auto mc_src : sources) {
+        bld.add_microslices(mc_src);
+    }
+
+    fles::TimesliceOutputArchive ar {"output.tsa"}; // TODO from argv
     while (auto ts_up = bld.get()) {
         ar.write(*ts_up);
     }
