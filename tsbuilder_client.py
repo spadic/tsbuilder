cbd4fb426c279a515a1d7ad058546de415926a81
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-09-22 13:17:56 +0200

    parameterized python script

--- tsbuilder_client.py
+++ tsbuilder_client.py
@@ -3,7 +3,7 @@ import struct
 import subprocess
 import zmq
 
-#---- wrapper for TimesliceBuilder ZMQ interface ---------------------
+#==== wrapper for TimesliceBuilder ZMQ interface =====================
 
 class TimesliceBuilder:
     def __init__(self, socket):
@@ -25,25 +25,27 @@ class TimesliceBuilder:
     def add_dtm(self, index, cbm_addr, data):
         self.send([0, index, cbm_addr] + data)
 
-#---- main -----------------------------------------------------------
+#==== simple use case (only one component) ===========================
 
-if __name__=='__main__':
-    p = subprocess.Popen(['./tsbuilder', '25', '0', 'spadic.tsa'])
+def run_single_component(ts_len, ts_start_idx, output_tsa,
+                         eq_id, sys_id, sys_ver, mc_start_idx,
+                         dtms_per_mc, words_per_dtm, input_file):
+    p = subprocess.Popen(['./tsbuilder',
+                          str(ts_len), str(ts_start_idx), output_tsa])
     c = zmq.Context()
     s = c.socket(zmq.PUSH)
     s.connect('ipc:///tmp/tsbuilder.ipc')
 
     t = TimesliceBuilder(s)
-    t.add_src(1, 0x40, 1, 0)
-    with open(os.path.expanduser('~/projekte/spadic10/misc/spadicdata/'
-                                 'dataStreamAnalogTrigger.dat')) as f:
+    t.add_src(eq_id, sys_id, sys_ver, mc_start_idx)
+    with open(input_file) as f:
         dtm = []
-        dtms_left = 0x100
+        dtms_left = dtms_per_mc
         for line in f:
             if not dtms_left:
                 t.next_mc(0)
-                dtms_left = 0x100
-            if len(dtm) == 32:
+                dtms_left = dtms_per_mc
+            if len(dtm) == words_per_dtm:
                 t.add_dtm(0, 0, dtm)
                 dtm = []
                 dtms_left -= 1
@@ -51,3 +53,21 @@ if __name__=='__main__':
 
     t.quit()
     p.communicate()
+
+if __name__=='__main__':
+    def get_arg(argv, option):
+        return argv[argv.index(option)+1]
+    from sys import argv
+
+    run_single_component(
+      ts_len = 25,
+      ts_start_idx = 0,
+      output_tsa = get_arg(argv, '-o'),
+      eq_id = 1,
+      sys_id = 0x40,
+      sys_ver = 1,
+      mc_start_idx = 0,
+      dtms_per_mc = 0x100,
+      words_per_dtm = 32,
+      input_file = get_arg(argv, '-i')
+    )
