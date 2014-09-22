f0488e7381d1e917d2791d6043537fbec7b89940
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-09-22 12:58:41 +0200

    execute the tsbuilder application from the python script

--- tsbuilder_client.py
+++ tsbuilder_client.py
@@ -1,5 +1,9 @@
+import os
 import struct
+import subprocess
 import zmq
+
+#---- wrapper for TimesliceBuilder ZMQ interface ---------------------
 
 class TimesliceBuilder:
     def __init__(self, socket):
@@ -21,10 +25,29 @@ class TimesliceBuilder:
     def add_dtm(self, index, cbm_addr, data):
         self.send([0, index, cbm_addr] + data)
 
+#---- main -----------------------------------------------------------
 
 if __name__=='__main__':
+    p = subprocess.Popen(['./tsbuilder', '25', '0', 'spadic.tsa'])
     c = zmq.Context()
     s = c.socket(zmq.PUSH)
     s.connect('ipc:///tmp/tsbuilder.ipc')
 
     t = TimesliceBuilder(s)
+    t.add_src(1, 0x40, 1, 0)
+    with open(os.path.expanduser('~/projekte/spadic10/misc/spadicdata/'
+                                 'dataStreamAnalogTrigger.dat')) as f:
+        dtm = []
+        dtms_left = 0x100
+        for line in f:
+            if not dtms_left:
+                t.next_mc(0)
+                dtms_left = 0x100
+            if len(dtm) == 32:
+                t.add_dtm(0, 0, dtm)
+                dtm = []
+                dtms_left -= 1
+            dtm.append(int(line, 16))
+
+    t.quit()
+    p.communicate()
